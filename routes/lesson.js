111f63f283de1ddfdbd4afb77c443eaf5d037515
kaiqigong <cagegong@gmail.com>
2016-01-27 22:59:53 +0800

    queue

--- routes/lesson.js
+++ routes/lesson.js
@@ -11,6 +11,7 @@ var moment = require('moment');
 var ffmpeg = require('fluent-ffmpeg');
 var Sentence = keystone.list('Sentence');
 var randomstring = require('randomstring');
+var async = require('async');
 
 qiniu.conf.ACCESS_KEY = '07cMjNhILyyOUOy4mes6SWwuwRnytDqrb6Zdlq0U';
 qiniu.conf.SECRET_KEY = 'NvlDby_4PcpNdWRfyzb5pli2y9mjquzC6Rv2GDnx';
@@ -32,78 +33,84 @@ router.get('/', middleware.requireUser, function(req, res, next) {
 function sliceAudio(lesson) {
 	var promises = [];
 	var srtPath = lesson.subtitle.path + '/' + lesson.subtitle.filename;
-	var data = fs.readFileSync(srtPath,'utf8');
+	var data = fs.readFileSync(srtPath, 'utf8');
+	fs.writeFileSync(srtPath, data, 'utf8');
+	data = fs.readFileSync(srtPath, 'utf8');
 	var srt = parser.fromSrt(data);
+	console.log(data);
 	var audioPath = lesson.audio.path + '/' + lesson.audio.filename;
-	srt.forEach(function(sub) {
-		// 	{ id: '1',
-		// startTime: '00:00:02,000',
-		// endTime: '00:00:06,000',
-		// text: 'Subtitle 1.1\n字幕 1.1' }
-		var promise = new Promise(function(resolve, reject) {
-			sub.startTime = sub.startTime.replace(',', '.');
-			sub.endTime = sub.endTime.replace(',', '.');
-			var duration = moment.duration(sub.endTime) - moment.duration(sub.startTime);
-			ffmpeg(audioPath)
-			.output(lesson.audio.path + '/' + lesson.courseNo + '_' + lesson.lessonNo + '_' + sub.id + '.mp3')
-			.seekInput(sub.startTime)
-			.duration(duration / 1000)
-			.on('error', function(err) {
-				console.log('An error occurred: ' + err.message);
-				reject();
-			})
-			.on('end', function() {
-				console.log('Processing finished !');
-				resolve(sub);
-			})
-			.run();
+	var promise = new Promise(function(resolve, reject) {
+		async.series(srt.map(function(sub) {
+			return function(callback) {
+				sub.startTime = sub.startTime.replace(',', '.');
+				sub.endTime = sub.endTime.replace(',', '.');
+				var duration = moment.duration(sub.endTime) - moment.duration(sub.startTime);
+				ffmpeg(audioPath)
+				.output(lesson.audio.path + '/' + lesson.courseNo + '_' + lesson.lessonNo + '_' + sub.id + '.mp3')
+				.seekInput(sub.startTime)
+				.duration(duration / 1000)
+				.on('error', function(err) {
+					console.log('An error occurred: ' + err.message);
+					callback(err);
+				})
+				.on('end', function() {
+					console.log('Processing finished !');
+					callback(null, sub);
+				})
+				.run();
+			};
+		}), function(err, results){
+			if (err) {
+				return reject(err);
+			}
+			return resolve(results);
 		});
-		promises.push(promise);
 	});
-	return Promise.all(promises);
+	return promise;
 }
 
 function generateSentences(lesson, subs) {
-	var promises = [];
-	subs.forEach(function(sub) {
-		// 	{ id: '1',
-		// startTime: '00:00:02,000',
-		// endTime: '00:00:06,000',
-		// text: 'Subtitle 1.1\n字幕 1.1' }
-		var promise = new Promise(function(resolve, reject) {
-			var sentence = new Sentence.model();
-			sentence.courseNo = lesson.courseNo;
-			sentence.lessonNo = lesson.lessonNo;
-			sentence.sentenceNo = parseInt(sub.id, 10);
-			sentence.english = sub.text.split('\n')[0];
-			sentence.chinese = sub.text.split('\n')[1];
-			// audios: { type: Types.TextArray },
-			var putPolicy = new qiniu.rs.PutPolicy('scott');
-			var uptoken = putPolicy.token();
-			var extra = new qiniu.io.PutExtra();
-			var qiniuPath = 'content/audios/' + sentence.courseNo + '/' + sentence.lessonNo + '/' + sentence.sentenceNo + '/' + randomstring.generate(6) + '.mp3';
-			var localPath = lesson.audio.path + '/' + lesson.courseNo + '_' + lesson.lessonNo + '_' + sub.id + '.mp3';
-			qiniu.io.putFile(uptoken,
-				qiniuPath,
-				localPath,
-				extra,
-				function(err, ret) {
-					if (err) {
-						return reject(err);
-					}
-					sentence.audios = [qiniuHost + '/' + qiniuPath];
-					sentence.save(function(err, result) {
+	return new Promise(function(resolve, reject) {
+		async.series(subs.map(function(sub) {
+			return function(callback) {
+				var sentence = new Sentence.model();
+				sentence.courseNo = lesson.courseNo;
+				sentence.lessonNo = lesson.lessonNo;
+				sentence.sentenceNo = parseInt(sub.id, 10);
+				sentence.english = sub.text.split('\n')[0];
+				sentence.chinese = sub.text.split('\n')[1];
+				// audios: { type: Types.TextArray },
+				var putPolicy = new qiniu.rs.PutPolicy('scott');
+				var uptoken = putPolicy.token();
+				var extra = new qiniu.io.PutExtra();
+				var qiniuPath = 'content/audios/' + sentence.courseNo + '/' + sentence.lessonNo + '/' + sentence.sentenceNo + '/' + randomstring.generate(6) + '.mp3';
+				var localPath = lesson.audio.path + '/' + lesson.courseNo + '_' + lesson.lessonNo + '_' + sub.id + '.mp3';
+				qiniu.io.putFile(uptoken,
+					qiniuPath,
+					localPath,
+					extra,
+					function(err, ret) {
 						if (err) {
-							return reject(err);
+							return callback(err);
 						}
-						resolve(result);
-					})
-				}
-			);
+						sentence.audios = [qiniuHost + '/' + qiniuPath];
+						sentence.save(function(err, result) {
+							if (err) {
+								return callback(err);
+							}
+							callback(null, result);
+						})
+					}
+				);
+
+			};
+		}), function(err, results) {
+			if (err) {
+				return reject(err);
+			}
+			resolve(results);
 		});
-		promises.push(promise);
 	});
-	return Promise.all(promises);
 }
 
 router.post('/', middleware.requireUser, function(req, res, next) {
@@ -154,8 +161,11 @@ router.post('/', middleware.requireUser, function(req, res, next) {
 	// 		);
 	// 	});
 	// })
-	.then(function(lesson) {
-		return sliceAudio(lesson);
+	.then(function() {
+		return Sentence.model.remove({courseNo: theLesson.courseNo, lessonNo: theLesson.lessonNo}).exec();
+	})
+	.then(function() {
+		return sliceAudio(theLesson);
 	})
 	.then(function(subs) {
 		return generateSentences(theLesson, subs);
