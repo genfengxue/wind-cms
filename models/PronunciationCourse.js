493f79d3b502483ac41ca7f1a34a1467e7cd7760
kaiqigong <cagegong@gmail.com>
2016-05-08 11:32:45 +0800

    upload image in pronunciation course

--- models/PronunciationCourse.js
+++ models/PronunciationCourse.js
@@ -1,5 +1,6 @@
 var keystone = require('keystone');
 var Types = keystone.Field.Types;
+var qiniuHelper = require('./qiniuHelper');
 
 /**
  * PronunciationCourse Model
@@ -15,7 +16,19 @@ PronunciationCourse.add({
 	englishTitle: { type: String},
 	chineseTitle: { type: String},
 	courseNo: { type: Number},
-	imageUrl: { type: String},
+	image: {
+		type: Types.LocalFile,
+		dest: '/data/files',
+		prefix: '/files',
+		note: '课程配图',
+	},
+	imageUrl: { 
+		type: String,
+		note: '自动生成，请勿修改',
+		hidden: true,
+		watch: 'image',
+		value: qiniuHelper('image'),
+	},
 	lessonCount: { type: Number},
 	description: { type: String},
 });
