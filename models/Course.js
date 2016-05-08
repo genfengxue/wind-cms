88cb294ce6b9472a948117ab6d8e1ffbf73902d5
kaiqigong <cagegong@gmail.com>
2016-05-08 17:19:21 +0800

    feat(style): courses lessons

--- models/Course.js
+++ models/Course.js
@@ -1,5 +1,6 @@
 var keystone = require('keystone');
 var Types = keystone.Field.Types;
+var qiniuHelper = require('./qiniuHelper');
 
 /**
  * Course Model
@@ -14,7 +15,33 @@ Course.add({
 	englishTitle: { type: String},
 	chineseTitle: { type: String},
 	courseNo: { type: Types.Number},
-	imageUrl: { type: String},
+	image: {
+		type: Types.LocalFile,
+		dest: '/data/files',
+		prefix: '/files',
+		note: '课程配图',
+	},
+	imageUrl: { 
+		type: String,
+		note: '自动生成，请勿修改',
+		hidden: true,
+		watch: 'image',
+		value: qiniuHelper('image'),
+	},
+	shareImage: {
+		type: Types.LocalFile,
+		dest: '/data/files',
+		prefix: '/files',
+		note: '分享配图，方的！尺寸大于300',
+	},
+	shareImageUrl: { 
+		type: String,
+		note: '自动生成，请勿修改',
+		hidden: true,
+		watch: 'shareImage',
+		value: qiniuHelper('shareImage'),
+	},
+	
 	description: { type: String},
 });
 
