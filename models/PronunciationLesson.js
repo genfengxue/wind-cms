2f1a0c8fe645385e17256aa697e4378aa7576498
kaiqigong <cagegong@gmail.com>
2016-05-09 17:54:56 +0800

    feat(lesson index): add lesson index

--- models/PronunciationLesson.js
+++ models/PronunciationLesson.js
@@ -11,6 +11,7 @@ var PronunciationLesson = new keystone.List('PronunciationLesson');
 PronunciationLesson.add({
 	courseNo: { type: Number, initial: true},
 	lessonNo: { type: Number, initial: true},
+	index: { type: Number, note: '顺序', initial: true},
 	englishTitle: { type: String, initial: true},
 	chineseTitle: { type: String, initial: true},
 	publishedDate: {type: Date, default: new Date(), note: '自动发布时间，默认是创建时间', initial: true}, // auto publish date
@@ -19,6 +20,8 @@ PronunciationLesson.add({
 PronunciationLesson.schema.virtual('link').get(function() {
 	return '/pronunciationLessons/' + this._id;
 });
+
+PronunciationLesson.schema.index({courseNo: 1, lessonNo: 1}, {unique: true});
 
 PronunciationLesson.defaultColumns = 'courseNo, lessonNo, englishTitle, chineseTitle, link|40%';
 PronunciationLesson.register();
